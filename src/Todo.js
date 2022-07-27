7df631e28ead9d2cbe07b45c0106f9980a356528
edtallison <edtallison@gmail.com>
2022-07-27 23:35:26 +1200

    Add clear completed functionality

--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
-export default function Todo({ todo }) {
-    return <div>{todo}</div>;
+export default function Todo({ todo, toggleTodo }) {
+    function handleTodoClick() {
+        toggleTodo(todo.id);
+    }
+
+    return (
+        <div>
+            <label>
+                <input type="checkbox" checked={todo.complete} onChange={handleTodoClick} />
+                {todo.name}
+            </label>
+        </div>
+    );
 }
