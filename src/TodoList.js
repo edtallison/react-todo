9645ce02250f702459b02efb5f1c2512dd024389
edtallison <edtallison@gmail.com>
2022-07-27 22:15:14 +1200

    Create Todo component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Todo from './Todo';
 
-export default function TodoList() {
-    return <div>Hello World</div>;
+export default function TodoList({ todos }) {
+    return todos.map((todo) => {
+        return <Todo key={todo} todo={todo} />;
+    });
 }
